e551732d8902dce51cea6af2f74e8eac8239ebf7
Richardlws <liu.bing66066@hotmail.com>
2025-04-19 23:49:09 +0800

    Creatmain

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // This is my first java program
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+         */
+        System.out.print("I like pizza!\n");
+        System.out.print("It's really good!\n");
+        System.out.print("Buy me pizza!");
+
+    }
+}
