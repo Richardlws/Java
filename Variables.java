eb85b66504fc7e66c32394d3b6f5646adcc283f3
Richardlws <liu.bing66066@hotmail.com>
2025-04-20 14:07:17 +0800

    variables

---
+++ Variables.java
@@ -0,0 +1,10 @@
+import java.sql.SQLSyntaxErrorException;
+
+public class Variables {
+    public static void main(String[] args) {
+        // variable = a reuseable container for a value
+        int age = 30;
+
+        System.out.println(age);
+    }
+}
