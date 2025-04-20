23820440ae995fd5636530261438646b9289dc7f
Richardlws <liu.bing66066@hotmail.com>
2025-04-20 14:11:57 +0800

    variables

--- Variables.java
+++ Variables.java
@@ -4,7 +4,19 @@ public class Variables {
     public static void main(String[] args) {
         // variable = a reuseable container for a value
         int age = 30;
+        int year = 2025;
+        int quantity = 1;
 
-        System.out.println(age);
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+
+
+        System.out.println(currency);
     }
 }
