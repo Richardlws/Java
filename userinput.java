37dbdf47d0af6367dabe6ee7f6df9b85cd913266
Richardlws <liu.bing66066@hotmail.com>
2025-04-20 21:05:18 +0800

    userinput

--- userinput.java
+++ userinput.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class userinput {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
+        /*Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
         System.out.print(("Enter your age: "));
@@ -25,5 +24,37 @@ public class userinput {
             System.out.println("You are NOT enrolled");
         }
         scanner.close();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter your favorite color: ");
+        String color = scanner.nextLine();
+
+        System.out.println("You are " + age + " years old.");
+        System.out.println("You like the color " + color);*/
+
+        //Calculate area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area is: " + area );
+
+        scanner.close();
+
+
     }
 }
