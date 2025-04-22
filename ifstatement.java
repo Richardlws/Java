c3096f969585012eb2910c75f30fac4cc5d5cdad
Richardlws <liu.bing66066@hotmail.com>
2025-04-22 13:46:01 +0800

    arithmeticoperators

---
+++ ifstatement.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ifstatement {
+    public static void main(String[] args) {
+
+        //if statement = performs a block of code if its condition is true
+
+        int age = 25;
+
+        if (age >= 18) {
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are a child");
+        }
+
+
+    }
+}
