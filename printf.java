e4a34e6e120602d833e24de2a4972d553d3dbf79
Richardlws <liu.bing66066@hotmail.com>
2025-04-24 06:29:22 +0800

    printf

---
+++ printf.java
@@ -0,0 +1,11 @@
+public class printf {
+    public static void main(String[] args){
+
+        //printf() = is a method used to format output
+
+        //%[flags][width][.precision][specifier-character]
+
+
+
+    }
+}
