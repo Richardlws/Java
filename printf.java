a4932c49b884c773c2536252aba96a93567771c4
Richardlws <liu.bing66066@hotmail.com>
2025-04-24 06:50:18 +0800

    printf

--- printf.java
+++ printf.java
@@ -1,11 +1,65 @@
 public class printf {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //printf() = is a method used to format output
 
         //%[flags][width][.precision][specifier-character]
 
+        String name = "Spongebob";
+        char firstLetter = 'S';
+        int age = 30;
+        double height = 60.5;
+        boolean isEmployed = true;
 
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("Your name starts with a %c\n", firstLetter);
+        System.out.printf("You are %d years old\n", age);
+        System.out.printf("You are %.1f inches tall\n", height);
+        System.out.printf("Employed: %b\n", isEmployed);
+        System.out.printf("%s is %d years old\n", name, age);
+
+        double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+        double price4 = 9000.99;
+        double price5 = 100000.15;
+        double price6 = -54000.01;
+
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        System.out.printf("%.2f\n", price1);
+        System.out.printf("%.2f\n", price2);
+        System.out.printf("%.2f\n", price3);
+
+        System.out.printf("%,+.2f\n", price4);
+        System.out.printf("%,+.2f\n", price5);
+        System.out.printf("%,+.2f\n", price6);
+
+        System.out.printf("%(.2f\n", price1);
+        System.out.printf("%(.2f\n", price2);
+        System.out.printf("%(.2f\n", price3);
+
+        System.out.printf("% .2f\n", price1);
+        System.out.printf("% .2f\n", price2);
+        System.out.printf("% .2f\n", price3);
+
+        System.out.printf("%04d\n", id1);
+        System.out.printf("%04d\n", id2);
+        System.out.printf("%04d\n", id3);
+        System.out.printf("%04d\n", id4);
+
+        System.out.printf("%4d\n", id1);
+        System.out.printf("%4d\n", id2);
+        System.out.printf("%4d\n", id3);
+        System.out.printf("%4d\n", id4);
+
+        System.out.printf("%-4d\n", id1);
+        System.out.printf("%-4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%-4d\n", id4);
 
     }
 }
