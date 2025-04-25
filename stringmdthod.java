57981b856b03457faa5cb0038fd23727cd0ce13b
Richardlws <liu.bing66066@hotmail.com>
2025-04-25 12:58:52 +0800

    stringmethod

---
+++ stringmdthod.java
@@ -0,0 +1,26 @@
+public class stringmdthod {
+    public static void main(String[] args){
+
+        String name = "";
+
+        //int length = name.length();
+        //char letter = name.charAt(0);
+        //int index = name.indexOf("o");
+        //int lastIndex = name.lastIndexOf("o");
+
+        //name = name.toUpperCase();
+        //name = name.toLowerCase();
+        //name = name.trim();
+        //name = name.replace("o","a");
+
+
+
+        //System.out.println(length);
+        //System.out.println(letter);
+        //System.out.println(index);
+        //System.out.println(lastIndex);
+        //System.out.println(name);
+        System.out.println(name.isEmpty());
+
+    }
+}
