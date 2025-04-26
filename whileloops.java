15edfad41a31ecb5a7af5c53c49d0c7c915725ee
Richardlws <liu.bing66066@hotmail.com>
2025-04-27 06:51:08 +0800

    whileloops

---
+++ whileloops.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class whileloops {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String name="";
+
+        while(name.isEmpty()){
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+        System.out.printf("Hello %s\n",name);
+    }
+}
