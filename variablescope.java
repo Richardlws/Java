f9f342405ce25812c8c2eb85d860c58ada1b4a03
Richardlws <liu.bing66066@hotmail.com>
2025-04-27 20:35:47 +0800

    overloadedmethods

---
+++ variablescope.java
@@ -0,0 +1,19 @@
+
+public class variablescope {
+
+    static int x = 3;
+
+    public static void main(String[] args){
+
+        int x = 1; //local
+
+        System.out.println(x);
+        doSomething();
+    }
+    static void doSomething(){
+        int x = 2; //local
+
+        System.out.println(x);
+
+    }
+}
