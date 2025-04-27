2d05ff8682e9b0098dc41ff792442e07eaed0de4
Richardlws <liu.bing66066@hotmail.com>
2025-04-27 20:28:55 +0800

    overloadedmethods

--- overloadedmethods.java
+++ overloadedmethods.java
@@ -1,11 +1,13 @@
 public class overloadedmethods {
     public static void main(String[] args) {
 
-        System.out.println(add(1, 2, 3));
-
+        //System.out.println(add(1, 2, 3, 4));
+        String pizza = bakePizza("flat bread", "mozzarella","pepperoni");
+        System.out.println(pizza);
 
     }
 
+    /*
     static double add(double a, double b) {
         return a + b;
     }
@@ -13,4 +15,21 @@ public class overloadedmethods {
     static double add(double a, double b, double c) {
         return a + b + c;
     }
+
+    static double add(double a, double b, double c, double d) {
+        return a + b + c + d;
+    }*/
+    static String bakePizza(String bread) {
+        return bread + " Pizza";
+    }
+
+    static String bakePizza(String bread, String cheese) {
+        return cheese + " " + bread + " Pizza";
+    }
+
+    static String bakePizza(String bread, String cheese, String topping) {
+        return topping + " " + cheese + " " + bread + " Pizza";
+    }
+
+
 }
