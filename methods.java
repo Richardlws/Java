b5a1e5c503791e472fb31cef92cfe097e84c2c20
Richardlws <liu.bing66066@hotmail.com>
2025-04-27 19:49:05 +0800

    whileloops

--- methods.java
+++ methods.java
@@ -1,15 +1,18 @@
+import javax.xml.namespace.QName;
 import java.util.Scanner;
 public class methods {
     public static void main(String[] args){
-        happyBirthday();
-        happyBirthday();
-        happyBirthday();
+        String name = "Bro";
+        int age = 21;
+
+        for (int i = 0;i<3;i++) {
+            happyBirthday(name,age);
+        }
     }
-    static void happyBirthday(){
+    static void happyBirthday(String name,int age){
         System.out.println("Happy Birthday to you!");
-        System.out.println("Happy Birthday dear you!");
-        System.out.println("You are x years old");
+        System.out.printf("Happy Birthday dear %s!\n",name);
+        System.out.printf("You are %d years old\n",age);
         System.out.println("Happy Birthday to you!\n");
     }
-
 }
