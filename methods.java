cd00779a7d53a81a600bd819351759ef88bbc451
Richardlws <liu.bing66066@hotmail.com>
2025-04-27 19:45:54 +0800

    whileloops

---
+++ methods.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class methods {
+    public static void main(String[] args){
+        happyBirthday();
+        happyBirthday();
+        happyBirthday();
+    }
+    static void happyBirthday(){
+        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday dear you!");
+        System.out.println("You are x years old");
+        System.out.println("Happy Birthday to you!\n");
+    }
+
+}
