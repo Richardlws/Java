2e572f5aaebe598110e8af19cde7db51bb5c1aa4
Richardlws <liu.bing66066@hotmail.com>
2025-04-27 12:46:03 +0800

    numberguessgame

---
+++ numberguessgame.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class numberguessgame {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Random random = new Random();
+        System.out.println(random);
+
+
+
+
+        scanner.close();
+
+    }
+}
