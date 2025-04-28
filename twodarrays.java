380381594329dabded5d1efaf61d9c2c203740e2
Richardlws <liu.bing66066@hotmail.com>
2025-04-28 21:39:35 +0800

    2darrays

--- twodarrays.java
+++ twodarrays.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
+
 public class twodarrays {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        /*
+        //String[] fruits = {"apple","orange","banana"};
+        //String[] vegetables = {"potato","onion","carrot"};
+        //String[] meats ={"chicken","pork","beef","fish"};
 
-        String[] fruits = {"apple","orange","banana"};
-        String[] vegetables = {"potato","onion","carrot"};
-        String[] meats ={"chicken","pork","beef","fish"};
+        String[][] groceries = { {"apple","orange","banana"},
+                                 {"potato","onion","carrot"},
+                                 {"chicken","pork","beef","fish"}};
+        //System.out.println(groceries[2][3]);
+        groceries[0][0]="pineapple";
+        groceries[1][0]="celery";
+        groceries[2][1]="eggs";
 
-        String[][] groceries = {fruits,vegetables,meats};
-        System.out.println(groceries[2][3]);
 
 
         for(String[] foods : groceries){
@@ -15,9 +22,20 @@ public class twodarrays {
                 System.out.print(food +" ");
             }
             //System.out.print(foods[1]+" ");
-            System.out.printf("\n");
-        }
+            System.out.print("\n");
+        }*/
 
+        char[][] telephone = {{'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'},
+                {'*', '0', '#'}};
+        for (char[] rows : telephone) {
+            for (char row : rows) {
+                System.out.print(row + " ");
+            }
+            System.out.print("\n");
+
+        }
 
     }
 }
