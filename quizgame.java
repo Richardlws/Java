319f0742c64808b71257eb77f34877b2439ee308
Richardlws <liu.bing66066@hotmail.com>
2025-04-29 07:04:28 +0800

    2darrays

---
+++ quizgame.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class quizgame {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        String[] question={"What is the main function of a router?",
+                            "Which part of the computer is considered the brain?",
+                            "What year was Facebook launched?",
+                            "Who is known as the father of computer?",
+                            "What was the first programming language?"};
+        String[][] options = {{"1.Storing files","2.Encrypting data","3.Directing internet traffic","4.Managing passwords"},
+                              {"1.CPU","2.Hard Drive","3.RAM","4.GPU"},
+                              {"1.2000","2.20004","3.2006","4.2008"},
+                              {"1.Steve Jobs","2.Bill Gates","3.Alan Turing","4.Charles Babbage"},
+                              {"1.COBOL","2.C","3.Fortran","4.Assembly"} ,
+                              };
+        System.out.println("******************************");
+        System.out.println("Welcome to the Java Quiz Game!");
+        System.out.println("******************************");
+        /*
+        System.out.println("What is the main function of a router?");
+        System.out.print("\n");
+        System.out.println("1.Storing files");
+        System.out.println("2.Encrypting data");
+        System.out.println("3.Directing internet traffic");
+        System.out.println("4.Managing passwords");
+        System.out.print("Your guess: ");
+        guess = scanner.nextInt();
+
+        xxxx(guess);
+        if(guess==3){
+            System.out.println("Yes! You are right!");
+        }
+        else{
+            System.out.println("Sorry! You are wrong!Think about it again");
+        }
+
+        scanner.close();
+    }
+    static boolean xxxx(int choice){
+        switch(choice){
+            case:1-> return false;*/
+
+        }
+
+    }
+}
