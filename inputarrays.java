507c16e00c00adbc9fe19ceae495752f063ad09c
Richardlws <liu.bing66066@hotmail.com>
2025-04-28 11:56:09 +0800

    array

--- inputarrays.java
+++ inputarrays.java
@@ -3,7 +3,12 @@ public class inputarrays {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-       String[] foods = new String[3];
+       //String[] foods = new String[3];
+       String[] foods;
+       int size;
+       System.out.print("What # of food do you want?: ");
+       size = scanner.nextInt();
+       foods = new String[size];
 
        for(int i = 0;i<foods.length;i++){
            System.out.print("Enter a food: ");
