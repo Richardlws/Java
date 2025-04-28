820fc851ae919aedab8301b0d29f7f39c1b21810
Richardlws <liu.bing66066@hotmail.com>
2025-04-28 11:37:07 +0800

    array

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args){
+
+        String[] fruit = {"apple","orange","banana","coconut"};
+        //fruit[0]= "pineapple";
+        for(int i = 0; i<4;i++) {
+
+            System.out.println(fruit[i]);
+    }
+
+    }
+}
