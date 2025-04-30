65a57db8009cda3a9b42dddb09af7ffdbba08f94
Richardlws <liu.bing66066@hotmail.com>
2025-04-30 17:19:29 +0800

    overloadconstructors

--- Car.java
+++ Car.java
@@ -1,28 +1,13 @@
-
 public class Car {
+    String model;
+    String color;
 
-    String name = "Ford";
-    String model = "Mustang";
-    int year = 2025;
-    double price = 58000.99;
-    boolean isRunning = false;
-
-    void start() {
-        isRunning = true;
-        System.out.println("You start the engine");
-    }
-
-    void stop() {
-        isRunning = false;
-        System.out.println("You stop the engine");
+    Car(String model, String color) {
+        this.model = model;
+        this.color = color;
     }
 
     void drive() {
-        System.out.println("You drive the " + model);
-
-    }
-
-    void brake() {
-        System.out.println("You stop the " + model);
+        System.out.println("You drive the " + this.color + " " + this.model);
     }
 }
