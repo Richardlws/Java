ac2d7125bf7a4825b9775d6c57bcc313243fb23c
Richardlws <liu.bing66066@hotmail.com>
2025-05-01 08:22:13 +0800

    quizgames

---
+++ Animal.java
@@ -0,0 +1,12 @@
+public class Animal extends Organism {
+    //boolean isAlive;
+
+    //Animal(){
+     //   isAlive=false;
+    //}
+
+    void eat(){
+        System.out.println("This animal is eating.");
+    }
+
+}
---
+++ inheritance.java
@@ -0,0 +1,32 @@
+public class inheritance {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        //animal.eat();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant =new Plant();
+
+        dog.speak();
+        cat.speak();
+
+        System.out.println(dog.lives);
+        System.out.println(cat.lives);
+
+        System.out.println("The dog is alive?: " + dog.isAlive);
+        System.out.println("The cat is alive?: " + cat.isAlive);
+
+        System.out.println(plant.isAlive);
+        plant.photosynthesize();
+
+
+
+     /*if(dog.isAlive){
+         dog.eat();
+     }
+     else{
+         System.out.println("Sorry! The dog is not alive!");
+     }*/
+
+
+    }
+}
