de1a4b689be4e8768a8dfcc4d142235e2cb40196
Richardlws <liu.bing66066@hotmail.com>
2025-05-05 10:04:34 +0800

    getterssetters

--- getterssetters/getterssetters.java
+++ getterssetters/getterssetters.java
@@ -2,6 +2,13 @@ package getterssetters;
 
 public class getterssetters {
     public static void main(String[] args){
+        Car car = new Car("Charge","Yellow",10000);
+        //car.model = "Corvette";
 
+        car.setModel("Corvette");
+        car.setColor("Blue");
+        car.setPrice(-100);
+
+        System.out.println(car.getColor()+" "+car.getModel()+" "+car.getPrice());
     }
 }
