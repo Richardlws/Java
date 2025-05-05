824e521cf50bfe65b6d3b7c1be753c25e53d4da0
Richardlws <liu.bing66066@hotmail.com>
2025-05-05 10:55:38 +0800

    Wrapper

--- Wrapper/Wrapper.java
+++ Wrapper/Wrapper.java
@@ -1,7 +1,28 @@
 package Wrapper;
 
 public class Wrapper {
-    public static void main(){
+    public static void main(String[] args) {
+        /*String a = Integer.toString(123);
+        String b = Double.toString(3.14);
+        String c = Character.toString('@');
+        String d = Boolean.toString(false);
+
+        String x = a + b + c + d;
+        System.out.println(x);
+
+        int a = Integer.parseInt("123");
+        double b = Double.parseDouble("3.14");
+        char c = "Pizza".charAt(0);
+        boolean d= Boolean.parseBoolean("true");
+
+        String x = a+b+c+d;
+        System.out.println(x);*/
+
+        char letter='B';
+        System.out.println(Character.isLetter(letter));
+
+        System.out.println(Character.isUpperCase(letter));
+
 
     }
 }
