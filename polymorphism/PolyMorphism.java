ec60dcd506ad46cf161797f776f908813ab43609
Richardlws <liu.bing66066@hotmail.com>
2025-05-05 09:27:23 +0800

    polymorphism

--- polymorphism/PolyMorphism.java
+++ polymorphism/PolyMorphism.java
@@ -1,7 +1,20 @@
 package polymorphism;
 
 public class PolyMorphism {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
+
+        /*car.go();
+        bike.go();
+        boat.go();*/
+
+        Vehicle[] vehicles = {car, bike, boat};
+
+        for(Vehicle vehicle : vehicles){
+            vehicle.go();
+        }
 
     }
 }
---
+++ polymorphism/Vehicletemp.java
@@ -0,0 +1,7 @@
+package polymorphism;
+
+public abstract class Vehicletemp {
+
+    abstract void go();
+
+}
