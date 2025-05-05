ed54c930aa15e5649e6ce2f213b7841aec34ad10
Richardlws <liu.bing66066@hotmail.com>
2025-05-05 11:09:34 +0800

    Wrapper

--- arraylists/arraylists.java
+++ arraylists/arraylists.java
@@ -1,6 +1,8 @@
 package arraylists;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class arraylists {
     public static void main(String[] args){
@@ -18,7 +20,7 @@ public class arraylists {
         list.add(1.0);
         list.add(2.2);
 
-        System.out.println(list);*/
+        System.out.println(list);
 
         ArrayList<String> fruits = new ArrayList<>();
 
@@ -27,7 +29,31 @@ public class arraylists {
         fruits.add("Apple");
         fruits.add("Coconut");
 
-        System.out.println(fruits);
+        //fruits.remove(0);
+        //fruits.set(0,"Pineapple");
+
+
+        //System.out.println(fruits.get(1));
+        //System.out.println(fruits.size());
+
+        //Collections.sort(fruits);
+
+        //System.out.println(fruits);
+
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> foods = new ArrayList<>();
+
+        System.out.println("Enter the # of food you would like: ");
+        int numofFood = scanner.nextInt();
+
+
+        scanner.close();
+
 
 
 
