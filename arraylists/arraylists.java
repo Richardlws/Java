ef13178d2e8df784a32bd691b4df7c70383c4e04
Richardlws <liu.bing66066@hotmail.com>
2025-05-05 11:02:53 +0800

    Wrapper

---
+++ arraylists/arraylists.java
@@ -0,0 +1,35 @@
+package arraylists;
+
+import java.util.ArrayList;
+
+public class arraylists {
+    public static void main(String[] args){
+        /*ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(3);
+        list.add(1);
+        list.add(2);
+
+        System.out.println(list);
+
+        ArrayList<Double> list = new ArrayList<>();
+
+        list.add(3.14);
+        list.add(1.0);
+        list.add(2.2);
+
+        System.out.println(list);*/
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Orange");
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Coconut");
+
+        System.out.println(fruits);
+
+
+
+    }
+}
