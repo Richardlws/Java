8908dc60016859c4f9e1ba38000d6332ed233422
Richardlws <liu.bing66066@hotmail.com>
2025-05-06 13:22:10 +0800

    readfile

--- ReadFile/ReadFile.java
+++ ReadFile/ReadFile.java
@@ -1,9 +1,31 @@
 package ReadFile;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+
 public class ReadFile {
     public static void main(String[] args){
 
+        String filePath = "C:\\Users\\Richard\\Desktop\\test.txt";
 
+
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            //System.out.println("That file exists");
+            String line;
+            while((line = reader.readLine())!=null){
+                System.out.println(line);
+            }
+
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        }
 
 
     }
