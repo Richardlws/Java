a03e73544ff2ef8f5f9bb31b58d6e0522a7fa472
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 15:12:39 +0800

    generics

--- generics/generics.java
+++ generics/generics.java
@@ -8,14 +8,18 @@ public class generics {
 
         fruits.add("apple");
         fruits.add("orange");
-        fruits.add("banana");*/
+        fruits.add("banana");
 
         Box<String> box = new Box<>();
 
         box.setItem("banana");
 
-        System.out.println(box.getItem());
+        System.out.println(box.getItem());*/
 
-
+        Product<String,Double>product1 = new Product<>("apple",0.50);
+        Product<String,Integer>product2 = new Product<>("ticket",15);
+        //System.out.println(product.getItem());
+        //System.out.println(product.getPrice());
+        System.out.println(product2.getPrice());
     }
 }
