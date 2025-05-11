7df8ca12cfd56bf6bdd61181f33973f7e3384636
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 09:52:41 +0800

    hangmangame

--- hangmangame/hangmangame.java
+++ hangmangame/hangmangame.java
@@ -1,7 +1,15 @@
 package hangmangame;
-import java.util.Scanner;
-public class hangmangame {
-    public static void  main(String[] args){
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class hangmangame {
+    public static void main(String[] args) {
+        String word = "pizza";
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Character>wordState = new ArrayList<>();
+
+
+        scanner.close();
     }
 }
