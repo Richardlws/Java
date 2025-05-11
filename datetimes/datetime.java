d5b877e95b6ad3b1298be6846fc8d4894eef9734
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 13:39:14 +0800

    datetime

--- datetimes/datetime.java
+++ datetimes/datetime.java
@@ -1,7 +1,19 @@
 package datetimes;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class datetime {
     public static void  main(String[] args){
-
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        LocalDateTime datetime = LocalDateTime.now();
+        Instant instant = Instant.now();
+        System.out.println(date);
+        System.out.println(time);
+        System.out.println(datetime);
+        System.out.println(instant);
     }
 }
