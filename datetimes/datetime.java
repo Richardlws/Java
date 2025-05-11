2b974f9cc00962b76244d7a1f0955f2e18ba4a6f
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 13:40:39 +0800

    datetime

--- datetimes/datetime.java
+++ datetimes/datetime.java
@@ -4,16 +4,21 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class datetime {
     public static void  main(String[] args){
-        LocalDate date = LocalDate.now();
+        /*LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
         LocalDateTime datetime = LocalDateTime.now();
         Instant instant = Instant.now();
         System.out.println(date);
         System.out.println(time);
         System.out.println(datetime);
-        System.out.println(instant);
+        System.out.println(instant);*/
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
     }
 }
