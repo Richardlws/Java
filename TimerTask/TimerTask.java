f417cab39b838528bd2ce845e06532fb1e1db2cf
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 14:43:36 +0800

    TimerTask

--- TimerTask/TimerTask.java
+++
@@ -1,7 +0,0 @@
-package TimerTask;
-
-public class TimerTask {
-    public static void main(String[] args){
-
-    }
-}
---
+++ TimerTasker/TimerTasker.java
@@ -0,0 +1,28 @@
+package TimerTasker;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerTasker {
+    public static void main(String[] args){
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+
+            int count = 3;
+
+            @Override
+            public void run(){
+                System.out.println("Hello!");
+                count--;
+                if(count==0){
+                    System.out.println("Task Complete!");
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.schedule(task,0,1000);
+
+
+    }
+}
