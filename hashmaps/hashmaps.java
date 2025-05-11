5e30f1b3cbf209c35ab26770091181a130142c0c
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 15:47:27 +0800

    hashmaps

--- hashmaps/hashmaps.java
+++ hashmaps/hashmaps.java
@@ -3,12 +3,12 @@ package hashmaps;
 import java.util.HashMap;
 
 public class hashmaps {
-    public static void main(String[] args){
-        HashMap<String,Double> map = new HashMap<>();
-        map.put("apple",0.5);
-        map.put("orange",0.75);
-        map.put("banana",0.25);
-        map.put("coconut",1.00);
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("apple", 0.5);
+        map.put("orange", 0.75);
+        map.put("banana", 0.25);
+        map.put("coconut", 1.00);
         //System.out.println(map);
         //map.remove("apple");
 
@@ -19,14 +19,18 @@ public class hashmaps {
 
         //System.out.println(map.containsKey("pineapple"));
 
-        if(map.containsKey("pineapple")){
+        /*if(map.containsKey("pineapple")){
             System.out.println(map.get("apple"));
         }
         else{
             System.out.println("Key not found");
+        }*/
+
+        //System.out.println(map.containsValue(1.00));
+        //System.out.println(map.size());
+        //System.out.println(map);
+        for (String key : map.keySet()) {
+            System.out.println(key + " : $" + map.get(key));
         }
-
-
-
     }
 }
