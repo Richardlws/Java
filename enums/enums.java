190c3895780f4cd749f4a6cf22c4b5235ef4bbdc
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 22:44:16 +0800

    enums

--- enums/enums.java
+++ enums/enums.java
@@ -1,11 +1,22 @@
 package enums;
 
+import java.sql.SQLOutput;
+
 public class enums {
     public static void main(String[] args){
 
         Day day= Day.SUNDAY;
-        System.out.println(day);
-        System.out.println(day.getDayNumber());
+        //System.out.println(day);
+        //System.out.println(day.getDayNumber());
+        switch(day){
+            case MONDAY,
+                 TUESDAY,
+                 WEDNESDAY,
+                 THURSDAY,
+                 FRIDAY -> System.out.println("It is a weekday");
+            case SATURDAY,SUNDAY-> System.out.println("It is the weekend");
+
+        }
 
     }
 }
