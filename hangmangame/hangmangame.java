2091e7ffdecafaf2cfcf627c13b7b0f620d401b5
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 10:04:09 +0800

    hangmangame

--- hangmangame/hangmangame.java
+++ hangmangame/hangmangame.java
@@ -11,18 +11,59 @@ public class hangmangame {
         ArrayList<Character> wordState = new ArrayList<>();
         int wrongGuesses = 0;
 
-        for(int i =0;i<word.length();i++){
+        for (int i = 0; i < word.length(); i++) {
             wordState.add('_');
         }
         //System.out.println(wordState);
         System.out.println("************************");
         System.out.println("Welcome to Java Hangman!");
         System.out.println("************************");
-
-
-
-
+        //System.out.println(getHangmanArt(2));
 
         scanner.close();
+    }
+
+    static String getHangmanArt(int WrongGuesses) {
+
+        return switch (WrongGuesses) {
+            case 0 -> """
+                    
+                    
+                    
+                    
+                    """;
+            case 1 -> """
+                     o
+                    
+                    
+                    """;
+            case 2 -> """
+                     o 
+                     |
+                    
+                    """;
+            case 3 -> """
+                     o 
+                    /|
+                    
+                    """;
+            case 4 -> """
+                     o 
+                    /|\\
+                    """;
+            case 5 -> """
+                     o 
+                    /|\\
+                    /  
+                    """;
+            case 6 -> """
+                     o 
+                    /|\\
+                    /  \\
+                    """;
+            default -> "";
+
+        };
+
     }
 }
