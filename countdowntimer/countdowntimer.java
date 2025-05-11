954ca18d5dc5fe1009266f35258d46891a9136c1
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 14:50:12 +0800

    countdown timer

--- countdowntimer/countdowntimer.java
+++ countdowntimer/countdowntimer.java
@@ -1,7 +1,26 @@
 package countdowntimer;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class countdowntimer {
     public static void main(String[] args){
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
 
+
+            int count = 5;
+
+            @Override
+            public void run() {
+                System.out.println(count);
+                count--;
+                if(count<0){
+                    System.out.println("Happy New Year!");
+                    timer.cancel();
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task,0,1000);
     }
 }
