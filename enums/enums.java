e424aaa20a6e1dc107a82ab5552e7b263d85e4ce
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 22:49:15 +0800

    enums

--- enums/enums.java
+++ enums/enums.java
@@ -1,23 +1,33 @@
 package enums;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class enums {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+        try {
 
-        Day day= Day.SUNDAY;
-        //System.out.println(day);
-        //System.out.println(day.getDayNumber());
-        switch(day){
-            case MONDAY,
-                 TUESDAY,
-                 WEDNESDAY,
-                 THURSDAY,
-                 FRIDAY -> System.out.println("It is a weekday");
-            case SATURDAY,SUNDAY-> System.out.println("It is the weekend");
+            Day day = Day.valueOf(response);
+            //Day day= Day.SUNDAY;
+            //System.out.println(day);
+            //System.out.println(day.getDayNumber());
+            switch(day){
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FRIDAY -> System.out.println("It is a weekday");
+                case SATURDAY,SUNDAY-> System.out.println("It is the weekend");
 
+            }
         }
-
+        catch(IllegalArgumentException e ){
+            System.out.println("Please enter a valid day");
+        }
+        scanner.close();
     }
 }
 
