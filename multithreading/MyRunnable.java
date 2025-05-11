af49f81fb89a58e3f2c733c59767f9d0a6acaac9
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 23:14:13 +0800

    multithreading

---
+++ multithreading/MyRunnable.java
@@ -0,0 +1,18 @@
+package multithreading;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run(){
+        for(int i =1;i<=5;i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println(i);
+            }
+
+            catch(InterruptedException e){
+                System.out.println("Thread was interrupted");
+            }
+
+        }
+    }
+}
