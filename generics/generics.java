3e2e54e55cd07d6ebfef87b168ffec239c49d8a0
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 15:01:19 +0800

    generics

--- generics/generics.java
+++ generics/generics.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 public class generics {
     public static void main(String[] args){
-        ArrayList<String> fruits= new ArrayList<>();
+        /*ArrayList<String> fruits= new ArrayList<>();
 
         fruits.add("apple");
         fruits.add("orange");
-        fruits.add("banana");
+        fruits.add("banana");*/
+
 
     }
 }
