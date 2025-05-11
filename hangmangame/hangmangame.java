c703dba25bcf45ddc34fd92fb445d25b0b484004
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 09:55:45 +0800

    hangmangame

--- hangmangame/hangmangame.java
+++ hangmangame/hangmangame.java
@@ -5,9 +5,22 @@ import java.util.ArrayList;
 
 public class hangmangame {
     public static void main(String[] args) {
-        String word = "pizza";
+        String word = "watermelon";
+
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Character>wordState = new ArrayList<>();
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for(int i =0;i<word.length();i++){
+            wordState.add('_');
+        }
+        //System.out.println(wordState);
+        System.out.println("************************");
+        System.out.println("Welcome to Java Hangman!");
+        System.out.println("************************");
+
+
+
 
 
         scanner.close();
