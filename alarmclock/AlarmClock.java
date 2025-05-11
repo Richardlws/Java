d1360b1aa9bf77a0a17c6385398043292cf5d113
Richardlws <liu.bing66066@hotmail.com>
2025-05-11 23:58:31 +0800

    alarmclock

--- alarmclock/AlarmClock.java
+++ alarmclock/AlarmClock.java
@@ -1,5 +1,6 @@
 package alarmclock;
 
+import java.awt.*;
 import java.time.LocalTime;
 
 public class AlarmClock implements Runnable {
@@ -18,19 +19,26 @@ public class AlarmClock implements Runnable {
             try {
                 Thread.sleep(1000);
 
-                int hours = LocalTime.now().getHour();
-                int minutes = LocalTime.now().getMinute();
-                int seconds = LocalTime.now().getSecond();
+                LocalTime now = LocalTime.now();
+
+                //int hours = now.getHour();
+                //int minutes = now.getMinute();
+                //int seconds = now.getSecond();
 
                 //System.out.println(hours + ":" + minutes + ":" + seconds);
-                System.out.printf("%02d:%02d:%02d",hours,minutes,seconds);
-                System.out.println();
+                System.out.printf("\r%02d:%02d:%02d",
+                                    now.getHour(),
+                                    now.getMinute(),
+                                    now.getSecond());
 
                 //System.out.println(LocalTime.now());
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
         }
+
+        System.out.println("\n*Alarm Noises*");
+        Toolkit.getDefaultToolkit().beep();
 
     }
 }
